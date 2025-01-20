878572bcceec733900d7bc1f63bd9f4999d47949
mozgbrasil <mozgbrasil@gmail.com>
2025-01-20 18:34:22 -0300

    base

--- docs/.vitepress/config/index.ts
+++ docs/.vitepress/config/index.ts
@@ -3,6 +3,7 @@ import { shared } from './shared'
 import { pt } from './pt'
 
 export default defineConfig({
+  base: 'https://mozgbrasil.github.io/vitepress-cerebrum/', // Substitua com o nome do seu repositório
   ...shared,
   locales: {
     root: { label: 'Português', ...pt }
